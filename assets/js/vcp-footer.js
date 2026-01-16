baf81615500f457cdd4e0a87ec789acc1f54661b
veritaschain <genspark_dev@genspark.ai>
2026-01-16 08:17:20 +0000

    Fix Bluesky icon: use inline SVG for reliable display

--- assets/js/vcp-footer.js
+++ assets/js/vcp-footer.js
@@ -42,8 +42,9 @@ class VCPFooter extends HTMLElement {
     constructor() {
         super();
 
-        // X (Twitter) SVG icon - inline for reliability
+        // SVG icons - inline for reliability
         this.xIconSvg = '<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>';
+        this.blueskyIconSvg = '<svg viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path d="M12 10.8c-1.087-2.114-4.046-6.053-6.798-7.995C2.566.944 1.561 1.266.902 1.565.139 1.908 0 3.08 0 3.768c0 .69.378 5.65.624 6.479.815 2.736 3.713 3.66 6.383 3.364.136-.02.275-.039.415-.056-.138.022-.276.04-.415.056-3.912.58-7.387 2.005-2.83 7.078 5.013 5.19 6.87-1.113 7.823-4.308.953 3.195 2.05 9.271 7.733 4.308 4.267-4.308 1.172-6.498-2.74-7.078a8.741 8.741 0 0 1-.415-.056c.14.017.279.036.415.056 2.67.297 5.568-.628 6.383-3.364.246-.828.624-5.79.624-6.478 0-.69-.139-1.861-.902-2.206-.659-.298-1.664-.62-4.3 1.24C16.046 4.748 13.087 8.687 12 10.8Z"/></svg>';
 
         // Social media links
         this.socialLinks = [
@@ -55,7 +56,7 @@ class VCPFooter extends HTMLElement {
             { name: 'Substack', url: 'https://substack.com/@tokachikamimura', icon: 'fa-solid fa-bookmark' },
             { name: 'ORCiD', url: 'https://orcid.org/0009-0002-0871-1627', icon: 'fa-brands fa-orcid' },
             { name: 'GitHub', url: 'https://github.com/veritaschain', icon: 'fa-brands fa-github' },
-            { name: 'Bluesky', url: 'https://bsky.app/profile/veritaschain.bsky.social', icon: 'fa-brands fa-bluesky' }
+            { name: 'Bluesky', url: 'https://bsky.app/profile/veritaschain.bsky.social', svg: this.blueskyIconSvg }
         ];
 
         // Translation data for all supported languages
